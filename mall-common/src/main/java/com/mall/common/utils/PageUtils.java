c4e6dd71b1f53d59a368918a21e98606c392cb55
wxc <1453716369@qq.com>
2022-10-18 23:48:54 +0800

    修改代码格式，增加部分样式

--- mall-common/src/main/java/com/mall/common/utils/PageUtils.java
+++ mall-common/src/main/java/com/mall/common/utils/PageUtils.java
@@ -7,19 +7,19 @@ import com.mall.common.utils.sql.SqlUtil;
 
 /**
  * 分页工具类
- * 
+ *
  * @author ruoyi
  */
-public class PageUtils extends PageHelper
-{
+public class PageUtils extends PageHelper {
     /**
      * 设置请求分页数据
      */
-    public static void startPage()
-    {
+    public static void startPage() {
+        // 根据request 的数据 构建分页对象( 只从request 中取数据，不对数据做任何处理 )
         PageDomain pageDomain = TableSupport.buildPageRequest();
         Integer pageNum = pageDomain.getPageNum();
         Integer pageSize = pageDomain.getPageSize();
+        //  检查获取的数据，防止sql注入
         String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
         Boolean reasonable = pageDomain.getReasonable();
         PageHelper.startPage(pageNum, pageSize, orderBy).setReasonable(reasonable);
@@ -28,8 +28,7 @@ public class PageUtils extends PageHelper
     /**
      * 清理分页的线程变量
      */
-    public static void clearPage()
-    {
+    public static void clearPage() {
         PageHelper.clearPage();
     }
 }
